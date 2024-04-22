6ea943514437b5815caf1e0f43b1f8cf1f967e77
TechTeam@50 <techteam50@TechTeam50s-MacBook-Pro.local>
2024-04-22 22:19:34 +0530

    Adding routing and dark mode functionality

--- React-JS/src/App.js
+++ React-JS/src/App.js
@@ -1,9 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from "./components/FirstComponent"
+import SecondComponent from "./components/SecondComponent"
+import ThirdComponent from "./components/ThirdComponent"
+import FourthComponent from "./components/FourthComponent"
 
+import {Route, BrowserRouter as Router , Routes, Routr} from 'react-router-dom'
+import Home from './routing/Home';
+import About from './routing/About';
+import Contact from './routing/Contact';
+import Navbar from './routing/Navbar';
+import DarkMode from './components/DarkMode';
 function App() {
   return (
-    <></>
+    <>
+    {/* <FirstComponent name="Atul" profession="Associate Sowtware Developer"/>
+    <SecondComponent/>
+    <ThirdComponent/>
+    <FourthComponent/> */}
+
+    {/* <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </Router> */}
+
+    {/* Dark mode Component */}
+    <DarkMode/>
+    </>
     
   );
 }
